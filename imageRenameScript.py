351df223b39bebab8995bb84cba2536aececedb1
JunHao9999 <h.junhao@yahoo.com.sg>
2023-07-30 04:09:41 +0800

    test

--- imageRenameScript.py
+++ imageRenameScript.py
@@ -6,13 +6,18 @@ def rename_files_increasing_numeric(folder_path):
         print("Folder path does not exist.")
         return
 
+    # Add other image extensions if needed
+    image_extensions = [".jpg", ".jpeg", ".png", ".gif"]
+
     files = os.listdir(folder_path)
-    files.sort()
+    image_files = [file for file in files if os.path.splitext(
+        file)[-1].lower() in image_extensions]
+    image_files.sort()
 
     counter = 1
-    for file in files:
-        file_extension = os.path.splitext(file)[-1]
-        new_file_name = f"{counter}{file_extension}"
+    for file in image_files:
+        file_extension = os.path.splitext(file)[-1].lower()
+        new_file_name = f"{counter}.jpg"  # Convert all images to JPG format
         old_file_path = os.path.join(folder_path, file)
         new_file_path = os.path.join(folder_path, new_file_name)
 
